4b9b212aa9c16133c3a03213a9ed5d162a31caec
Yanky Hoffman <developer.yankyhoffman@gmail.com>
2025-05-07 19:08:18 -0400

    Type annotations

--- src/checkgen/models.py
+++ src/checkgen/models.py
@@ -1,15 +1,18 @@
+from datetime import date, datetime
+from decimal import Decimal
+
 from num2words import num2words
 from num2words.currency import parse_currency_parts
 
 
 class Bank:
-    def __init__(self, name, routing_number):
+    def __init__(self, name: str, routing_number: str) -> None:
         self.name = name
         self.routing_number = routing_number
 
 
 class Issuer:
-    def __init__(self, name, details, bank, account_number):
+    def __init__(self, name: str, details: str | None, bank: Bank, account_number: str) -> None:
         self.name = name
         self.details = details
         self.bank = bank
@@ -17,21 +20,21 @@ class Issuer:
 
 
 class Payment:
-    def __init__(self, payee, amount, date, memo=None):
+    def __init__(self, payee: str, amount: float | Decimal, date: datetime | date, memo=None) -> None:
         self.payee = payee
         self._amount = amount
         self._date = date
         self.memo = memo
 
     @property
-    def amount(self):
+    def amount(self) -> str:
         return f"{self._amount:,.2f}"
 
     @property
-    def date(self):
+    def date(self) -> str:
         return f"{self._date.month}/{self._date.day}/{self._date.year}"
 
-    def written_amount(self):
+    def written_amount(self) -> str:
         dollars, cents, _ = parse_currency_parts(self._amount, is_int_with_cents=False)
 
         dollars = num2words(dollars).replace(' and', '').replace(',', '').replace('-', ' ')
